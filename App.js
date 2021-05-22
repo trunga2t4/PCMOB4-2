ab391014a9322a2b116cf0617fdbfea8d91b72c3
Trung <trunga2t4@gmail.com>
2021-05-23 00:34:55 +0800

    Clean up unused code

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import AddScreen from "./screens/AddScreen";
